6d327e6a4e2ccbff244c16deab25f6e0127f03e1
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2017-05-24 18:11:16 +0200

    First commit

---
+++ da_pak/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace da_pak
+{
+    class Pak
+    {
+        static void Initialize() {
+            TcpListener server = null;
+            try
+            {
+                Int16 port = 1300;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                server = new TcpListener(localAddr, port);
+                server.Start();
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                while(!false)
+                {
+                    System.Console.WriteLine("Waiting for a connection... ");
+
+                    /* Perf. blocking call to accpt requests */
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    NetworkStream stream = client.GetStream();
+                    client.Close();
+                }
+            }
+            catch(SocketException e)        { Console.WriteLine("\nSocketException: {0}", e);                       }
+            catch(System.IO.IOException e)  { Console.WriteLine("\nClient'z connection was interrupted!\n{0}", e);  }
+            finally
+            {
+                server.Stop();
+            }
+
+            System.Console.WriteLine("\nHit Enter to continue...");
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            Initialize();
+        }
+    }
+}
---
+++ da_rat/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace da_rat
+{
+    class Rat
+    {
+        static void Connect(String server, String message)
+        {
+            TcpClient client = null;
+            try
+            {
+                Int16 port = 1300;
+                client = new TcpClient(server, port);
+
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Stream stream = client.GetStream();
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0}", message);
+
+                data = new Byte[256];
+
+                String responseData = String.Empty;
+
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+            }
+            catch (ArgumentNullException e)   { Console.WriteLine("\nArgumentNullException: {0}", e);   }
+            catch (SocketException e)         { Console.WriteLine("\nServer not found! Closing... "); System.Environment.Exit(e.ErrorCode); }
+            finally
+            {
+                client.Close();
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            string mahssage = "Dickbutt";
+            Connect("127.0.0.1", mahssage);
+        }
+    }
+}
