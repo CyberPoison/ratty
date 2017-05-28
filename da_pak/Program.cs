529141a6d0bb0387cca13e309ff106df2af2288d
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2017-05-28 20:00:12 +0000

    Major refractor. TODO: Client-Side Receive-Async Task

--- da_pak/Program.cs
+++ da_pak/Program.cs
@@ -1,52 +1,107 @@
 ﻿using System;
+using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace da_pak
 {
     class Pak
     {
-        static void Initialize() {
-            TcpListener server = null;
-            try
-            {
-                Int16 port = 1300;
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-                server = new TcpListener(localAddr, port);
-                server.Start();
-
-                Byte[] bytes = new Byte[256];
-                String data = null;
-
-                while(!false)
-                {
-                    System.Console.WriteLine("Waiting for a connection... ");
-
-                    /* Perf. blocking call to accpt requests */
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
-
-                    data = null;
-
-                    NetworkStream stream = client.GetStream();
-                    client.Close();
-                }
-            }
-            catch(SocketException e)        { Console.WriteLine("\nSocketException: {0}", e);                       }
-            catch(System.IO.IOException e)  { Console.WriteLine("\nClient'z connection was interrupted!\n{0}", e);  }
-            finally
-            {
-                server.Stop();
-            }
-
-            System.Console.WriteLine("\nHit Enter to continue...");
-            Console.Read();
-        }
-
+        private static byte[] _buffer = new byte[2048];
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static Socket _serverSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
         static void Main(string[] args)
         {
-            Initialize();
+            Console.Title = "Pak";
+            
+            SetupServer();
+            SendLoop();
+
+            Console.WriteLine("Exiting...");
+            Console.ReadLine();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 1337));
+            
+            _serverSocket.Listen(5);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = _serverSocket.EndAccept(AR);
+            
+            _clientSockets.Add(socket);
+            Console.WriteLine("Client connected...");
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            
+            try
+            {
+                int received = socket.EndReceive(AR);
+
+                byte[] dataBuf = new byte[received];
+                Array.Copy(_buffer, dataBuf, received);
+
+                string text = Encoding.ASCII.GetString(dataBuf);
+                Console.WriteLine("Text received: {0}", text); 
+
+                // HANDLE REQUEST
+
+                string response = string.Empty;
+                if (text.ToLower() != "get time")
+                {
+                    response = "Invalid Request";
+                }
+                else 
+                {
+                    response = DateTime.Now.ToLongTimeString();
+                }
+
+                byte[] data = Encoding.ASCII.GetBytes(response);
+                socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+
+                socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            }
+            catch (SocketException) { Console.WriteLine("Client unexpectedly disconnected..."); }
+        }
+
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
+        private static void SendLoop()
+        {
+            while(true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine().Trim();
+
+                if (input == "break" || input == "exit")
+                    break;
+                
+                byte[] data = Encoding.ASCII.GetBytes(input);
+                
+                foreach(var sok in _clientSockets)
+                    sok.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), sok);
+                
+            }
         }
     }
 }
--- da_rat/Program.cs
+++ da_rat/Program.cs
@@ -1,50 +1,101 @@
 ﻿using System;
+using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
+using System.Threading;
 
 namespace da_rat
 {
     class Rat
     {
-        static void Connect(String server, String message)
-        {
-            TcpClient client = null;
-            try
-            {
-                Int16 port = 1300;
-                client = new TcpClient(server, port);
-
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                // Stream stream = client.GetStream();
-                NetworkStream stream = client.GetStream();
-
-                stream.Write(data, 0, data.Length);
-                Console.WriteLine("Sent: {0}", message);
-
-                data = new Byte[256];
-
-                String responseData = String.Empty;
-
-                int bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
-            }
-            catch (ArgumentNullException e)   { Console.WriteLine("\nArgumentNullException: {0}", e);   }
-            catch (SocketException e)         { Console.WriteLine("\nServer not found! Closing... "); System.Environment.Exit(e.ErrorCode); }
-            finally
-            {
-                client.Close();
-            }
-
-            Console.WriteLine("\n Press Enter to continue...");
-            Console.Read();
-        }
-
+        private static ManualResetEvent _connectDone = new ManualResetEvent(false);
+        private static Socket _clientSocket =
+            new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
         static void Main(string[] args)
         {
-            string mahssage = "Dickbutt";
-            Connect("127.0.0.1", mahssage);
+            Console.Title = "Rat";
+
+            // LoopConnect();
+            ConnectAsync();
+            SendLoop();
+
+            Console.WriteLine("Exiting...");
+            Console.ReadLine();
+        }
+
+        private static void ConnectAsync(string home = "127.0.0.1", int port = 1337)
+        {
+            _clientSocket.BeginConnect(IPAddress.Parse(home), port
+                , new AsyncCallback(ConnectCallback), _clientSocket);
+
+            _connectDone.WaitOne();
+        }
+
+        private static void ConnectCallback(IAsyncResult AR)
+        {
+            try
+            {
+                /* Retreive the socket from the state obj & complete conn. */
+                Socket client = (Socket)AR.AsyncState;
+                client.EndConnect(AR);
+
+                Console.WriteLine("Socket connected to {0}",
+                    client.RemoteEndPoint.ToString());
+
+                _connectDone.Set();
+            } catch (Exception e) { Console.WriteLine(e.ToString()); }
+        }
+
+        /* TODO: RECEIVE CALLBACK
+        ***********************************************************/
+
+/* OldConn
+        private static void LoopConnect()
+        {
+            int attempts = 0;
+
+            while(!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts += 1;
+                    _clientSocket.Connect(IPAddress.Loopback, 1337); // Change loopback with main IP
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Connection attempts: {0}", attempts.ToString());
+                }
+
+                Console.Clear();
+                Console.WriteLine("Connected...");
+            }
+        }
+*/
+
+        private static void SendLoop()
+        {
+            while(true)
+            {
+                Console.Write("> ");
+                string req = Console.ReadLine().Trim();
+
+                if (req == "break" || req == "exit")
+                    break;
+
+                byte[] buffer = Encoding.ASCII.GetBytes(req);
+                _clientSocket.Send(buffer);
+
+                byte[] recBuf = new byte[2048];
+                int rec = _clientSocket.Receive(recBuf);
+
+                byte[] data = new byte[rec];
+                Array.Copy(recBuf, data, rec);
+
+                Console.WriteLine("Received: {0}", Encoding.ASCII.GetString(data));
+            }
         }
     }
 }
